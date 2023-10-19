b8fb9d690ae7659558a365adac3b67e4ca819588
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2023-10-19 17:47:55 +0300

    update

--- src/index.js
+++
@@ -1,6 +0,0 @@
-const hello = require('./js/hi');
-import './sass/index.scss';
-import appleImg from './assets/images/aple.png';
-
-const image = document.getElementById('image');
-console.log('image:', image);
