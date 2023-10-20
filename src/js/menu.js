326997d55c780e992e929a2551b74bfbe8ab577e
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2023-10-20 12:45:47 +0300

    init

---
+++ src/js/menu.js
@@ -0,0 +1,16 @@
+const openMenuBTN = document.getElementById("menu-open-btn");
+console.log("openMenuBTN:", openMenuBTN);
+const closeMenuBTN = document.getElementById("menu-close-btn");
+console.log("closeMenuBTN:", closeMenuBTN);
+
+const menu = document.querySelector(".menu");
+console.log("menu:", menu);
+
+const handleOpenMenu = () => menu.classList.add("open");
+
+const handleCloseMenu = ({ target }) =>
+  target.matches("a") ||
+  (target.matches("svg") && menu.classList.remove("open"));
+
+openMenuBTN.addEventListener("click", handleOpenMenu);
+closeMenuBTN.addEventListener("click", handleCloseMenu);
