c08fad68a1db9f61551054c439017b25b0fddf93
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-11-19 13:47:15 +0200

    add order form

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,9 +1,13 @@
 const orderBtn = document.getElementById("order-btn");
 const backdrop = document.getElementById("backdrop");
+const closeModalBtn = document.getElementById("close-modal");
 
 const handleOpenModal = (e) => {
-  console.log("click");
   backdrop.classList.add("openModal");
+};
+const handleCloseModal = (e) => {
+  backdrop.classList.remove("openModal");
 };
 
 orderBtn.addEventListener("click", handleOpenModal);
+closeModalBtn.addEventListener("click", handleCloseModal);
