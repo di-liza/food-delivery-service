d37552d46930be8050bc58233c90a83444884356
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-11-19 17:48:36 +0200

    modal close

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,12 +2,17 @@ const orderBtn = document.getElementById("order-btn");
 const backdrop = document.getElementById("backdrop");
 const closeModalBtn = document.getElementById("close-modal");
 
-const handleOpenModal = (e) => {
+const handleOpenModal = () => {
   backdrop.classList.add("openModal");
+  document.body.style.overflow = "hidden";
 };
-const handleCloseModal = (e) => {
+const handleCloseModal = ({ code }) => {
   backdrop.classList.remove("openModal");
+  document.body.style.overflow = "scroll";
+  code === "Escape" && backdrop.classList.remove("openModal");
 };
 
 orderBtn.addEventListener("click", handleOpenModal);
 closeModalBtn.addEventListener("click", handleCloseModal);
+backdrop.addEventListener("click", handleCloseModal);
+window.addEventListener("keydown", handleCloseModal);
