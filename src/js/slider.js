20b4c2631fd6cbe94542c075d14428b90c04e947
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2023-11-26 15:28:40 +0200

    addad slider functionality for desktop -v

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,14 +1,23 @@
 let offset = 0;
 const sliderLine = document.querySelector(".slider-line");
 
+let windowWidth = null;
+
 document.getElementById("next-btn").addEventListener("click", () => {
-  offset += 320;
-  if (offset > 640) offset = 0;
+  windowWidth = window.innerWidth;
+
+  windowWidth >= 1280 ? (offset += 580) : (offset += 320);
+  if (offset === 1740 || offset === 960) offset = 0;
+
   sliderLine.style.left = `-${offset}px`;
 });
-document.getElementById("prev-btn").addEventListener("click", () => {
-  if (offset <= 0) offset = 960;
-  offset -= 320;
 
+document.getElementById("prev-btn").addEventListener("click", () => {
+  windowWidth = window.innerWidth;
+
+  if (offset <= 0)
+    offset = windowWidth >= 1280 ? (offset = 1740) : (offset += 960);
+
+  windowWidth >= 1280 ? (offset -= 580) : (offset -= 320);
   sliderLine.style.left = `-${offset}px`;
 });
