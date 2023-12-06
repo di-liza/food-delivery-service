b6cc06b6e74afc744ca56ad9de06b1d0f3dc6b70
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-12-06 17:25:26 +0200

    add slider interval

--- src/js/slider.js
+++ src/js/slider.js
@@ -3,16 +3,27 @@ const sliderLine = document.querySelector(".slider-line");
 
 let windowWidth = null;
 
-document.getElementById("next-btn").addEventListener("click", () => {
+const startInterval = () =>
+  setInterval(() => {
+    nextSlider();
+  }, 3000);
+let intervalId = startInterval();
+
+const nextSlider = () => {
+  clearInterval(intervalId);
   windowWidth = window.innerWidth;
 
   windowWidth >= 1280 ? (offset += 580) : (offset += 320);
   if (offset === 1740 || offset === 960) offset = 0;
 
   sliderLine.style.left = `-${offset}px`;
-});
+  intervalId = startInterval();
+};
+
+document.getElementById("next-btn").addEventListener("click", nextSlider);
 
 document.getElementById("prev-btn").addEventListener("click", () => {
+  clearInterval(intervalId);
   windowWidth = window.innerWidth;
 
   if (offset <= 0)
@@ -20,4 +31,5 @@ document.getElementById("prev-btn").addEventListener("click", () => {
 
   windowWidth >= 1280 ? (offset -= 580) : (offset -= 320);
   sliderLine.style.left = `-${offset}px`;
+  intervalId = startInterval();
 });
