a936b047dfe28810225ab0e13b94485c82d8655a
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-12-07 11:07:32 +0200

    form styles with validation (row -v)

--- src/js/orderForm.js
+++ src/js/orderForm.js
@@ -3,6 +3,7 @@ const inputsEl = document.querySelectorAll("#order-form-input");
 const errorMessagesEl = document.querySelectorAll(".order-form__error-message");
 const errorFormMessage = document.querySelector(".error__message-pop-up");
 const backdrop = document.getElementById("backdrop");
+const submitBtn = document.getElementById("order-submit-btn");
 
 let error = null;
 
@@ -18,26 +19,53 @@ const resetForm = () => {
   });
 };
 
-const handleInputChange = ({ target: { value, name } }) => {
+const handleInputChange = ({ target }) => {
+  const { value, name } = target;
+  if (value.trim() === "") error = true;
   errorMessagesEl.forEach((message) => {
     if (value.trim() === "" && message.id === name) {
       message.classList.add("open");
-      error = !error;
+      error = true;
+      target.classList.add("input--error");
     } else if (message.id === name) {
       const isValid = value.match(regexPatterns[name]);
       message.classList[isValid ? "remove" : "add"]("open");
-      if (isValid) error = false;
+      if (isValid) {
+        error = false;
+        target.classList.remove("input--error");
+      }
     }
+    if (error) submitBtn.classList.add("order-form__submit--error");
+    else submitBtn.classList.remove("order-form__submit--error");
   });
 };
 inputsEl.forEach((i) => i.addEventListener("input", handleInputChange));
 inputsEl.forEach((i) => i.addEventListener("blur", handleInputChange));
 
+const isFormFilled = () => {
+  let isFilled = false;
+
+  inputsEl.forEach((input) => {
+    if (input.required && input.value.trim() === "") {
+      isFilled = true;
+      input.classList.remove("input--error");
+    } else {
+      input.classList.add("input--error");
+    }
+  });
+
+  return isFilled;
+};
 const handleFormSubmit = (e) => {
+  const formFilled = isFormFilled();
+  console.log("formFilled:", formFilled);
   e.preventDefault();
-  if (error) errorFormMessage.style.display = "block";
-  else if (error === false) {
+  if (error || !formFilled) {
+    errorFormMessage.style.display = "block";
+    submitBtn.classList.add("order-form__submit--error");
+  } else if (error === false && formFilled) {
     resetForm();
+    submitBtn.classList.remove("order-form__submit--error");
     backdrop.classList.remove("openModal");
     document.body.style.overflow = "scroll";
     alert(
