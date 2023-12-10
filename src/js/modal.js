f9dc0620673b1553bda0b2cf53fffb499450b836
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-12-10 19:39:51 +0200

    slider optimization

--- src/js/modal.js
+++ src/js/modal.js
@@ -20,6 +20,7 @@ const handleCloseModal = ({ target, currentTarget, code }) => {
   }
 };
 
+console.log("orderBtn:", orderBtn);
 orderBtn.forEach((btn) => btn.addEventListener("click", handleOpenModal));
 closeModalBtn.addEventListener("click", handleCloseModal);
 backdrop.addEventListener("click", handleCloseModal);
