896fe1dfc7b2454c6a78ac41adca3c3247becd24
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-12-11 21:40:55 +0200

    fix form empty error

--- src/js/updateErrorState.js
+++ src/js/updateErrorState.js
@@ -1,26 +1,26 @@
 import refs from "./refs";
 import regexPatterns from "./regexPatterns";
 
-const { orderFormInputs, submitBtn, errorFormMessage } = refs;
+const { submitBtn } = refs;
 
-// const inputRef = [...orderFormInputs].map((input) => input);
-
-const updateErrorState = (error, input) => {
+const updateErrorState = (error, input, submit) => {
   const name = input.name;
   const errorMessages = document.querySelectorAll(
     `[data-error-type="${name}"]`
   );
-
   errorMessages.forEach((message) => {
     const isValid =
-      input.value.trim() !== "" && input.value.match(regexPatterns[name]);
+      !!input.value.trim() && input.value.match(regexPatterns[name]);
 
     message.classList.toggle("order-form__error-message--show", !isValid);
     input.classList.toggle("input--error", !isValid);
+    error = !isValid;
   });
 
   submitBtn.classList.toggle("order-form__submit--error", error);
-  errorFormMessage.classList.toggle("error__message-pop-up--show", error);
+
+  if (submit) {
+  }
 };
 
 export default updateErrorState;
