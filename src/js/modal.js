4d92333f495977e7f3de3bcf266fc0f79e5baa4f
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2023-12-21 21:13:10 +0200

    fix modal open/close animation

--- src/js/modal.js
+++ src/js/modal.js
@@ -5,33 +5,28 @@ const { modal, orderBtn, backdrop, closeModalBtn } = refs;
 const handleOpenModal = () => {
   modal.classList.add("active");
   backdrop.classList.add("openModal");
+  backdrop.classList.remove("is-hidden");
   document.body.style.overflow = "hidden";
 };
 
 const handleCloseModal = ({ target, currentTarget, code }) => {
   const closeBtnEl = target.closest("#close-modal");
-  modal.classList.remove("active");
 
   if (
     target === currentTarget ||
     code === "Escape" ||
     currentTarget === closeBtnEl
   ) {
+    modal.classList.remove("active");
     backdrop.classList.remove("openModal");
+    backdrop.classList.add("is-hidden");
     document.body.style.overflow = "scroll";
   }
 };
 
-console.log([...orderBtn].map((btn) => btn));
-console.log(document.querySelector(".js-orderBtn-ts"));
-
-[...orderBtn].map((btn) => {
+orderBtn.forEach((btn) => {
   btn.addEventListener("click", handleOpenModal);
 });
 closeModalBtn.addEventListener("click", handleCloseModal);
 backdrop.addEventListener("click", handleCloseModal);
 window.addEventListener("keydown", handleCloseModal);
-
-document.querySelector(".openPopup").addEventListener("click", () => {
-  document.querySelector(".popup").classList.add("acrivepop");
-});
