603c9d85a42e9a668f189830c4eb32e9c667c5de
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2024-03-17 15:49:26 +0200

    add close menu animation

--- src/js/menu.js
+++ src/js/menu.js
@@ -6,15 +6,15 @@ const menu = document.querySelector(".mob-menu");
 
 const handleOpenMenu = () => {
   menu.classList.add("open");
-  menu.classList.remove("swing-out-top-bck");
   menu.classList.add("swing-in-top-bck");
+  menu.classList.remove("swing-out-top-fwd");
 };
 
 const handleCloseMenu = ({ target }) => {
   if (target.matches("a") || target.matches("svg")) {
-    // menu.classList.remove("swing-in-top-bck");
-    menu.classList.add("swing-out-top-bck");
-    menu.classList.remove("open");
+    menu.classList.remove("swing-in-top-bck");
+    menu.classList.add("swing-out-top-fwd");
+    menu.classList.contains("swing-out-top-fwd")?.menu.classList.remove("open");
   }
 };
 
